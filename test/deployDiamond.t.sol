f666e973c86c3e86f5a4d84399f3b136657eeabf
AMD <michojekunle1@gmail.com>
2024-10-30 09:02:01 +0100

    deployment test

--- test/deployDiamond.t.sol
+++ test/deployDiamond.t.sol
@@ -5,9 +5,15 @@ import "../contracts/interfaces/IDiamondCut.sol";
 import "../contracts/facets/DiamondCutFacet.sol";
 import "../contracts/facets/DiamondLoupeFacet.sol";
 import "../contracts/facets/OwnershipFacet.sol";
+import "../contracts/facets/NFTCollateralFacet.sol";
+import "../contracts/facets/LoanFacet.sol";
 import "../contracts/Diamond.sol";
 
 import "./helpers/DiamondUtils.sol";
+
+contract TestNft is ERC721 {
+    
+}
 
 contract DiamondDeployer is DiamondUtils, IDiamondCut {
     //contract types of facets to be deployed
@@ -15,6 +21,8 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
     DiamondCutFacet dCutFacet;
     DiamondLoupeFacet dLoupe;
     OwnershipFacet ownerF;
+    NFTCollateralFacet nftCltF;
+    LoanFacet loanF;
 
     function testDeployDiamond() public {
         //deploy facets
@@ -22,11 +30,13 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
         diamond = new Diamond(address(this), address(dCutFacet));
         dLoupe = new DiamondLoupeFacet();
         ownerF = new OwnershipFacet();
+        nftCltF = new NFTCollateralFacet();
+        loanF = new LoanFacet();
 
         //upgrade diamond with facets
 
         //build cut struct
-        FacetCut[] memory cut = new FacetCut[](2);
+        FacetCut[] memory cut = new FacetCut[](4);
 
         cut[0] = (
             FacetCut({
@@ -44,12 +54,33 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
             })
         );
 
+        cut[2] = (
+            FacetCut({
+                facetAddress: address(nftCltF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("NFTCollateralFacet")
+            })
+        );
+
+        cut[3] = (
+            FacetCut({
+                facetAddress: address(loanF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("LoanFacet")
+            })
+        );
+
         //upgrade diamond
         IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
 
         //call a function
         DiamondLoupeFacet(address(diamond)).facetAddresses();
     }
+
+    // tests for nft collateral and loan management features
+    function test_NFT_Collateral() public {}
+    
+    function test_Loan_Management() public {}
 
     function diamondCut(
         FacetCut[] calldata _diamondCut,
